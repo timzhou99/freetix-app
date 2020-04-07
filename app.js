0022d2a009d5c70e710a7e7b11b95caedd245fa5
Timothy Zhou <39917965+timzhou99@users.noreply.github.com>
2020-04-07 01:41:56 -0400

    Created form. Backup before testing out EJS.

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ require('./db');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const qrcode = require('qrcode');
+
+const flash = require('express-session');
 const passport = require('passport');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+
+const qrcode = require('qrcode');
 
 const app = express();
+
+const User = mongoose.model('User');
+const Event = mongoose.model('Event');
+const Ticket = mongoose.model('Ticket');
 
 // enable sessions
 const session = require('express-session');
 const sessionOptions = {
     secret: 'secret cookie thang (store this elsewhere!)',
-    resave: true,
-    saveUninitialized: true
+    resave: false,
+    saveUninitialized: false
 };
 app.use(session(sessionOptions));
 
@@ -27,9 +36,87 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+//authentication routes
+
 app.get('/', (req, res) => {
-    res.render('index');
+    res.redirect('/login');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        new User({
+            username: req.body.username,
+            password: hashedPassword,
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            userCity: req.body.city,
+            userState: req.body.state
+        }).save(function(err){
+
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            res.redirect('/login');
+
+        });
+
+    } catch {
+        res.redirect('/register');
+    }
+
+});
+
+//routes
+
+app.get('/events', (req, res) => {
+    res.render('events');
+});
+
+app.get('/events/create', (req, res) => {
+    res.render('create');
+});
+
+app.post('/events/create', (req, res) => {
+
+    res.redirect('/events/hosting')
+});
+
+app.get('/events/hosting', (req, res) => {
+    res.render('hosting');
+});
+
+app.get('/events/:eventID', (req, res) => {
+    res.render('event');
+});
+
+app.get('/events/:eventID/manage', (req, res) => {
+    res.render('manage');
+});
+
+app.get('/tickets', (req, res) => {
+    res.render('tickets');
+});
+
+app.get('/tickets/:ticketID', (req, res) => {
+    res.render('manage');
 });
 
 app.listen(3000);
-console.log("server started on port: 3000");
+console.log("server started on localhost:3000");
--- db.js
+++ db.js
@@ -6,39 +6,18 @@ const mongoose = require('mongoose');
 // * they also can have 0 or more events
 // * they can also have 0 or more tickets
 const User = new mongoose.Schema({
-    // username provided by authentication plugin
-    // password hash provided by authentication plugin
+
+    email: {type:String, required: true},
+    password: {type:String, required: true},
+
+    firstName: {type:String, required: true},
+    lastName: {type:String, required: true},
+
     events:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Event' }],
     tickets:  [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ticket' }],
 
     userCity: {type: String, required: true},
     userState: {type: String, required: true}
-});
-
-// an event
-// * includes properties that pertain to the event
-// * a event is associated with a User
-// * a event has a list of Tickets
-// * when an event has passed its date and end time, it is Not Active
-const Event = new mongoose.Schema({
-    eventManager: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
-
-    eventName: {type: String, required: true},
-    eventDescription: {type: String, required: true},
-    eventPicture: {type: String, required: true},
-    eventActive: {type: Boolean, default: true, required: false},
-
-    eventDate: {type: Date, required: true},
-    eventTimeStart: {type: Date, required: true},
-    eventTimeEnd: {type: Date, required: true},
-
-    eventAddress: {type: String, required: true},
-    eventCity: {type: String, required: true},
-    eventState: {type: String, required: true},
-
-    maxQuantity: {type: Number, required: true},
-    currentQuantity: {type: Number, required: true},
-    tickets: [Ticket]
 }, {
     _id: true
 });
@@ -56,9 +35,64 @@ const Ticket = new mongoose.Schema({
     ticketValue: {type: Number, required: true},
     ticketStatus: {type: String, required: true},
 
-    createdAt: {type: Date, required: true}
+    eventDate: {type: Date, required: true}
+
+    //createdAt: {type: Date, required: true, default: Date.now}
 }, {
     _id: true
 });
+
+// an event
+// * includes properties that pertain to the event
+// * a event is associated with a User
+// * a event has a list of Tickets
+// * when an event has passed its date and end time, it is Not Active
+const Event = new mongoose.Schema({
+    eventManager: {type: mongoose.Schema.Types.ObjectId, ref:'User'},
+
+    eventName: {type: String, required: true},
+    eventDescription: {type: String, required: true},
+    eventPicture: {type: String, required: true},
+    eventActive: {type: Boolean, default: true, required: false},
+
+    eventStart: {type: Date, required: true},
+    eventEnd: {type: Date, required: true},
+
+    eventAddress: {type: String, required: true},
+    eventCity: {type: String, required: true},
+    eventState: {type: String, required: true},
+
+    maxQuantity: {type: Number, required: true},
+    currentQuantity: {type: Number, required: true},
+    tickets: [Ticket]
+}, {
+    _id: true
+});
+
+mongoose.model('User', User);
+mongoose.model('Event', Event);
+mongoose.model('Ticket', Ticket);
+
+// is the environment variable, NODE_ENV, set to PRODUCTION?
+let dbconf;
+if (process.env.NODE_ENV === 'PRODUCTION') {
+    // if we're in PRODUCTION mode, then read the configration from a file
+    // use blocking file io to do this...
+    const fs = require('fs');
+    const path = require('path');
+    const fn = path.join(__dirname, '/config.json');
+    const data = fs.readFileSync(fn);
+
+    // our configuration file will be in json, so parse it and set the
+    // connection string appropriately!
+    const conf = JSON.parse(data);
+    dbconf = conf.dbconf;
+
+} else {
+    // if we're not in PRODUCTION mode, then use
+    dbconf = 'mongodb+srv://freetix_app:6baJPEE8zmX3@test-cluster1-ucy6v.mongodb.net/test?retryWrites=true&w=majority';
+}
+
+mongoose.connect(dbconf, { useNewUrlParser : true, useUnifiedTopology : true});
 
 // TODO: add remainder of setup for slugs, connection, registering models, etc. below
